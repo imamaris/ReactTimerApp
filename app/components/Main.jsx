aa06ef3bed3ebe6ccca9c55372cfe14065306b0c
imamaris <imam.aris94@gmail.com>
2018-02-14 17:06:38 +0700

    Add navigation and fixing

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,13 +4,12 @@ var Navigation = require('Navigation');
 var Main = (props) => {
   return (
     <div>
-      <Navigation />
       <div>
         <div>
-          <p>Main component</p>
+          <Navigation />
+          <p>Main.jsx Rendered</p>
           {props.children}
         </div>
-        
       </div>
     </div>
   );
